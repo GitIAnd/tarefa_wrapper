829acbc0036549f8c0fb0b25b640f1bdb0c90e4b
Andre <andreituassu3@gmail.com>
2024-01-04 12:11:07 -0300

    criando variável primitiva e wrapper

---
+++ src/br/com/api/TarefaWrapper.java
@@ -0,0 +1,12 @@
+package br.com.api;
+
+public class TarefaWrapper {
+
+        public static void main(String args []) {
+
+            int anoAtual = 2024;
+            System.out.println(anoAtual);
+            Integer anoAtual2 = Integer.valueOf(anoAtual);
+            System.out.println(anoAtual2);
+        }
+}
